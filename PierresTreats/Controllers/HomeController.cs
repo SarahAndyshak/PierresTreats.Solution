b9a3fb6e1a43f88bd00645bb85b3d99c50ddb279
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-24 14:57:25 -0700

    WIP can't get Flavors pages to show up. Tried deleting and remaking Views/Flavor/Index, still doesn't work.

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,40 +1,3 @@
-// using Microsoft.AspNetCore.Mvc;
-// using PierresTreats.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.AspNetCore.Identity;
-// using System.Threading.Tasks;
-// using System.Security.Claims;
-
-// namespace PierresTreats.Controllers
-// {
-//   public class HomeController : Controller
-//   {
-//     private readonly PierresTreatsContext _db;
-//     private readonly UserManager<ApplicationUser> _userManager;
-//     public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
-//     {
-//       _userManager = userManager;
-//       _db = db;
-//     }
-
-//     [HttpGet("/")]
-//     public async Task<ActionResult> Index()
-//     {
-//       //Treat logic
-//       Treat[] treats = _db.Treats.ToArray();
-//       Dictionary<string, object[]> model = new Dictionary<string, object[]>();
-//       model.Add("treats", treats);
-      
-//       //Flavor logic
-//       Flavor[] flavors = _db.Flavors.ToArray();
-//       model.Add("flavors", flavors);
-
-//       return View(model);
-//     }
-//   }
-// }
-
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using System.Collections.Generic;
@@ -55,6 +18,22 @@ namespace PierresTreats.Controllers
       _db = db;
     }
 
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
+    }
+  }
+}
+
+
+// Original code gave warning because of async -- not needed when no treats or flavors are associated with individual users:
+
     // [HttpGet("/")]
     // public async Task<ActionResult> Index()
     // {
@@ -70,16 +49,3 @@ namespace PierresTreats.Controllers
 
     //   return View(model);
     // }
-
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      Treat[] treats = _db.Treats.ToArray();
-      Flavor[] flavors = _db.Flavors.ToArray();
-      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
-      model.Add("treats", treats);
-      model.Add("flavors", flavors);
-      return View(model);
-    }
-  }
-}
