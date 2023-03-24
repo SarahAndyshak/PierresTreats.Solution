6777e7d29f61199ed4a5b16a25a8a0fed173e80b
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-03-24 10:29:23 -0700

    WIP confused about why some using directives aren't underlined, but also don't show the namepsace when hovered over.

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+  }
+}
